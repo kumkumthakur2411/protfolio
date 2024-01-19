3b5178a2fcaf1013fc24aa2e6dfe90302edf7918
kumkumthakur2411 <157138629+kumkumthakur2411@users.noreply.github.com>
2024-01-19 21:50:44 +0530

    main javascript file

---
+++ main.js
@@ -0,0 +1,15 @@
+/*  */
+/* var typed = new Typed(".text",{
+    Strings:"web developer",
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+}); */
+var typed = new Typed('.text', {
+    strings: ['Frontend Developer', 'Full stack Web developer.', ],
+    typeSpeed: 50,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+  });
